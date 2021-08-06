3fce506b1edfc78cc459e1c0d553a429af2f3d21
cuonggio003 <nguyenquoccuongvntn2003@gmail.com>
2021-08-06 14:56:15 +0700

    index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>thời gian hiện tại</title>
+</head>
+<body>
+    <h1> bây giờ là: <?php echo date('Y-M-d h:m:d');?></h1>
+    <?php date_default_timezone_set('Asian/Ho_Chi_Minh')?>
+</body>
+</html>
