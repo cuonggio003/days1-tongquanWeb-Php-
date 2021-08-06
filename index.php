1d18090d0db171c1e0ba87dd52b550be8eaaf92f
cuonggio003 <nguyenquoccuongvntn2003@gmail.com>
2021-08-06 15:19:28 +0700

    index

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>thời gian hiện tại</title>
-</head>
-<body>
-    <h1> bây giờ là: <?php echo date('Y-M-d h:m:d');?></h1>
-    <?php date_default_timezone_set('Asian/Ho_Chi_Minh')?>
-</body>
+<html>
+    <style type="text/css">
+    .login {
+        height:200px; width:250px;
+        margin:0;
+        padding: 25px;
+        border:1px #CCC solid;
+    }
+    .login input{
+        padding:5px; margin:5px;
+
+    }
+  </style>
+  <body>
+      <form>
+          <div class="login">
+              <h2>Login</h2>
+              <input type="text" name="username" size="30" placeholder="username" />
+              <input type="password" name="password" size="30" placeholder="password"/>
+              <input tyte="submit" value="Sign in" />
+            </form>
+    <?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST["username"]; 
+        $password = $_POST["password"];
+         
+        if ($username === "admin" && $password === "admin") {	 
+             echo "<h2>Welcome <span style='color:red'>" .$username. "</span> to website</h2>";
+        } else{
+             echo "<h2><span style='color:red'>Login Error</span></h2>";
+        }
+     }
+     ?>      
+  </body>
 </html>
